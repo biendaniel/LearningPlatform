efc060657ec84d1ea9be0302ab55c4217c52781d
biendaniel <daniel.bien01@gmail.com>
2019-06-09 22:53:00 +0200

    add tests in CourseDao

--- src/test/java/dao/CourseDaoTest.java
+++ src/test/java/dao/CourseDaoTest.java
@@ -6,9 +6,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.hamcrest.Matchers;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,21 +21,98 @@ import static org.mockito.BDDMockito.given;
 @RunWith(MockitoJUnitRunner.class)
 public class CourseDaoTest {
 
-    private CourseDao courseDao;
+    @Mock
+    CourseDao courseDao;
 
-    @Before
-    public void init() {
-        courseDao = new CourseDao();
-    }
+
     @Test
-    public void should_return_pass_result() {
+    public void getAvarangeOpinionsTestShouldReturnCorrectResult() {
+        //given
+        courseDao = new CourseDao();
         //when
+        System.out.println(getOpinionCourseMock());
         double rating = courseDao.getAvarageOpinions(getOpinionCourseMock());
         //then
         Assert.assertEquals(rating, 2.0, 0.1);
     }
 
-    public static List<CourseOpinion> getOpinionCourseMock() {
+    @Test
+    public void getAvarangeOpinionsTestShouldReturnWrongResult1() {
+        //when
+        courseDao = new CourseDao();
+        double rating = courseDao.getAvarageOpinions(getOpinionCourseMock());
+        //then
+        Assert.assertNotEquals(rating, 2.1, 0.1);
+    }
+
+    @Test
+    public void getAvarangeOpinionsTestShouldReturnWrongResult2() {
+        //when
+        courseDao = new CourseDao();
+        double rating = courseDao.getAvarageOpinions(getOpinionCourseMock());
+        //then
+        Assert.assertNotEquals(rating, 1.9, 0.1);
+    }
+
+    @Test
+    public void findAllTestShouldReturnCorrectLengthOfList() {
+        //given
+        given(courseDao.findAll()).willReturn(getCoursesMock());
+        final int LIST_LENGTH = 5;
+        //when
+        List<Course> courses = courseDao.findAll();
+        //then
+        Assert.assertThat(courses, Matchers.hasSize(LIST_LENGTH));
+    }
+
+    @Test
+    public void findAllTestShouldReturnWrongLengthOfList1() {
+        //given
+        given(courseDao.findAll()).willReturn(getCoursesMock());
+        final int LIST_LENGTH = 4;
+        //when
+        List<Course> courses = courseDao.findAll();
+        //then
+        Assert.assertNotEquals(courses, Matchers.hasSize(LIST_LENGTH));
+    }
+
+    @Test
+    public void findAllTestShouldReturnWrongLengthOfList2() {
+        //given
+        given(courseDao.findAll()).willReturn(getCoursesMock());
+        final int LIST_LENGTH = 6;
+        //when
+        List<Course> courses = courseDao.findAll();
+        //then
+        Assert.assertNotEquals(courses, Matchers.hasSize(LIST_LENGTH));
+    }
+
+    @Test
+    public void findByIdTestShouldReturnEqualsCourseName() {
+        //given
+        final String COURSE_NAME = "Matma I";
+        given(courseDao.findById(anyInt())).willReturn(getCourseMock());
+        //when
+        Course course = courseDao.findById(1);
+        //then
+        Assert.assertEquals(course.getName(), COURSE_NAME);
+    }
+
+    @Test
+    public void findByIdTestShouldReturnNotEqualsCourseName() {
+        //given
+        final String COURSE_NAME = "Matma 1";
+        given(courseDao.findById(anyInt())).willReturn(getCourseMock());
+        //when
+        Course course = courseDao.findById(1);
+        //then
+        Assert.assertNotEquals(course.getName(), COURSE_NAME);
+    }
+
+
+
+
+    public List<CourseOpinion> getOpinionCourseMock() {
         List<CourseOpinion> opinions = new ArrayList<>();
         CourseOpinion courseOpinion1 = new CourseOpinion();
         courseOpinion1.setValue(1);
@@ -49,6 +129,28 @@ public class CourseDaoTest {
         opinions.add(courseOpinion4);
 
         return opinions;
+    }
+
+    public List<Course> getCoursesMock() {
+
+        List<Course> courses = new LinkedList<>();
+        courses.add(new Course());
+        courses.add(new Course());
+        courses.add(new Course());
+        courses.add(new Course());
+        courses.add(new Course());
+        System.out.println(courses.size());
+        courses.get(0).setName("aaa");
+        return courses;
+
+    }
+
+    public Course getCourseMock() {
+        Course course = new Course();
+        course.setId(1);
+        course.setName("Matma I");
+        course.setOpinions(getOpinionCourseMock());
+        return course;
     }
 
 }
